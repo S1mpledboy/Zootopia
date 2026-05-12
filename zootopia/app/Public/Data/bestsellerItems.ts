c80fae15366aa3c0300d06abc7c6af85bfd7ed39
Makar Oliinyk <deffffff4@gmail.com>
2026-05-13 00:48:26 +0200

    Revert "ID UPD"
    This reverts commit 483ae39edcbe7faddd5d746a93330b06d13ae44d.

--- zootopia/app/Public/Data/bestsellerItems.ts
+++ zootopia/app/Public/Data/bestsellerItems.ts
@@ -3,7 +3,7 @@ import item2 from "@/app/Public/Images/item2.png";
 
 export const BestsellersItems = [
   {
-    id: "69d926427931d1d5ad56c0d4",
+    id: 1,
     brandName: "Karma wołowa 15kg",
     productName: "Dobra karma",
     price: 229,
@@ -11,7 +11,7 @@ export const BestsellersItems = [
     image: item1,
   },
   {
-    id: "69d926427931d1d5ad56c0d4",
+    id: 2,
     brandName: "Karma kurczak 10kg",
     productName: "Lepsza karma",
     price: 199,
@@ -19,10 +19,10 @@ export const BestsellersItems = [
     image: item2,
   },
   {
-    id: "69d926427931d1d5ad56c0d4",
-    brandName: "Karma wołowa 15kg",
-    productName: "Dobra karma",
-    price: 229,
+    id: 3,
+    brandName: "Karma kurczak 10kg",
+    productName: "Lepsza karma",
+    price: 199,
     promoPrice: null,
     image: item2,
   },
