def4a20a4307ca34f3c28d3228f6355a734394e0
Makar Oliinyk <deffffff4@gmail.com>
2026-05-18 19:31:51 +0200

    Update Product.js

--- zootopia/models/Product.js
+++ zootopia/models/Product.js
@@ -29,6 +29,11 @@ const ProductSchema = new mongoose.Schema(
       ref: "Category",
       required: [true, "Category is required"],
     },
+    company: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Company",
+      required: [true, "Company is required"],
+    },
     isActive: {
       type: Boolean,
       default: true,
