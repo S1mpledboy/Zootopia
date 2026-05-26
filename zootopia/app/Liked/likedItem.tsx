0b4c4f617f870c3cda74d226033426fad0269822
Makar Oliinyk <deffffff4@gmail.com>
2026-05-26 15:28:39 +0200

    Update likedItem.tsx

--- zootopia/app/Liked/likedItem.tsx
+++ zootopia/app/Liked/likedItem.tsx
@@ -4,7 +4,7 @@ import type { NextPage } from 'next';
 import Image from 'next/image';
 
 import styles from './liked.module.css';
-import ProductImage from '@/app/Public/Images/kotMokra.png';  // ← Dodaj /app
+import ProductImage from '@/app/Public/Images/kotMokra.png';
 import FavoriteIcon from '@/app/Public/Images/Vector.svg';
 import CartIcon from '@/app/Public/Images/tabler-icon-shopping-bag-plus.svg';
 
@@ -20,34 +20,62 @@ const Property1Ulubione: NextPage = () => {
 			/>
 
 			<div className={styles.frameParent}>
-				{/* ... reszta kodu ... */}
+				<div className={styles.frameGroup}>
+					<div className={styles.frameWrapper}>
+						<div className={styles.krainaNoteciParent}>
+							<b className={styles.krainaNoteci}>
+								KRAINA NOTECI
+							</b>
 
-				<div className={styles.ulubioneParent}>
-					<Image 
-						src={FavoriteIcon}
-						alt="Ulubione" 
-						className={styles.ulubioneIcon}
-						width={24}
-						height={24}
-					/>
+							<div className={styles.karmaLoremIpsum}>
+								KARMA LOREM IPSUM 5KG
+							</div>
+						</div>
+					</div>
 
-					<div className={styles.dodajDoKoszyka}>
+					<div className={styles.zWrapper}>
+						<div className={styles.z}>178,00 zł</div>
+					</div>
+				</div>
+
+				<div className={styles.frameContainer}>
+					<div className={styles.parent}>
+						<div className={styles.div}>-</div>
+
+						<div className={styles.wrapper}>
+							<div className={styles.div2}>1</div>
+						</div>
+
+						<div className={styles.div3}>+</div>
+					</div>
+
+					<div className={styles.ulubioneParent}>
 						<Image 
-							src={CartIcon}
-							alt="Koszyk" 
-							className={styles.vectorIcon}
+							src={FavoriteIcon}
+							alt="Ulubione" 
+							className={styles.ulubioneIcon}
+							width={24}
+							height={24}
+						/>
+
+						<div className={styles.dodajDoKoszyka}>
+							<Image 
+								src={CartIcon}
+								alt="Koszyk" 
+								className={styles.vectorIcon}
+								width={24}
+								height={24}
+							/>
+						</div>
+
+						<Image 
+							src={FavoriteIcon}
+							alt="Ulubione" 
+							className={styles.ulubioneIcon}
 							width={24}
 							height={24}
 						/>
 					</div>
-
-					<Image 
-						src={FavoriteIcon}
-						alt="Ulubione" 
-						className={styles.ulubioneIcon}
-						width={24}
-						height={24}
-					/>
 				</div>
 			</div>
 		</div>
