2ce9594f51a658f4d810708c93c8b3bcfcccef55
Makar Oliinyk <deffffff4@gmail.com>
2026-05-26 15:24:15 +0200

    Update likedItem.tsx

--- zootopia/app/Liked/likedItem.tsx
+++ zootopia/app/Liked/likedItem.tsx
@@ -1,72 +1,53 @@
 "use client";
+
 import type { NextPage } from 'next';
 import Image from 'next/image';
 
-import styles from './liked.module.css';  // ← Ścieżka względna!
+import styles from './liked.module.css';
+import ProductImage from '@/Public/Images/kotMokra.png';  // ← Bez `/app`
+import FavoriteIcon from '@/Public/Images/Vector.svg';
+import CartIcon from '@/Public/Images/tabler-icon-shopping-bag-plus.svg';
 
 const Property1Ulubione: NextPage = () => {
 	return (
 		<div className={styles.property1ulubione}>
 			<Image
 				className={styles.imgProduktuIcon}
-				src="/Public/Images/kotMokra.png"  // ← Ścieżka zamiast importu
+				src={ProductImage}
 				width={100}
 				height={100}
 				alt="Produkt"
 			/>
 
 			<div className={styles.frameParent}>
-				<div className={styles.frameGroup}>
-					<div className={styles.frameWrapper}>
-						<div className={styles.krainaNoteciParent}>
-							<b className={styles.krainaNoteci}>
-								KRAINA NOTECI
-							</b>
+				{/* ... reszta kodu ... */}
 
-							<div className={styles.karmaLoremIpsum}>
-								KARMA LOREM IPSUM 5KG
-							</div>
-						</div>
-					</div>
+				<div className={styles.ulubioneParent}>
+					<Image 
+						src={FavoriteIcon}
+						alt="Ulubione" 
+						className={styles.ulubioneIcon}
+						width={24}
+						height={24}
+					/>
 
-					<div className={styles.zWrapper}>
-						<div className={styles.z}>178,00 zł</div>
-					</div>
-				</div>
-
-				<div className={styles.frameContainer}>
-					<div className={styles.parent}>
-						<div className={styles.div}>-</div>
-
-						<div className={styles.wrapper}>
-							<div className={styles.div2}>1</div>
-						</div>
-
-						<div className={styles.div3}>+</div>
-					</div>
-
-					<div className={styles.ulubioneParent}>
-						{/* SVG jako <img> tag */}
-						<img 
-							src="/Public/Images/Vector.svg" 
-							alt="Ulubione" 
-							className={styles.ulubioneIcon} 
-						/>
-
-						<div className={styles.dodajDoKoszyka}>
-							<img 
-								src="/Public/Images/tabler-icon-shopping-bag-plus.svg" 
-								alt="Koszyk" 
-								className={styles.vectorIcon} 
-							/>
-						</div>
-
-						<img 
-							src="/Public/Images/Vector.svg" 
-							alt="Ulubione" 
-							className={styles.ulubioneIcon} 
+					<div className={styles.dodajDoKoszyka}>
+						<Image 
+							src={CartIcon}
+							alt="Koszyk" 
+							className={styles.vectorIcon}
+							width={24}
+							height={24}
 						/>
 					</div>
+
+					<Image 
+						src={FavoriteIcon}
+						alt="Ulubione" 
+						className={styles.ulubioneIcon}
+						width={24}
+						height={24}
+					/>
 				</div>
 			</div>
 		</div>
