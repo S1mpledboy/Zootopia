7c85943cf99d6177dcd2ade8c2c7628a8fce031b
Makar Oliinyk <deffffff4@gmail.com>
2026-05-26 14:13:38 +0200

    Likied Test

---
+++ zootopia/app/Liked/likedItem.tsx
@@ -0,0 +1,68 @@
+import type { NextPage } from 'next';
+
+import ProductImage from "@app/Public/Images/product-image.png";
+
+import FavoriteIcon from "@app/Public/Images/ulubione-icon.svg";
+import CartIcon from "@app/Public/Images/tabler-icon-shopping-bag-plus.svg";
+
+import Image from 'next/image';
+
+import styles from './liked.css';
+
+const Property1Ulubione: NextPage = () => {
+	return (
+		<div className={styles.property1ulubione}>
+			<Image
+				className={styles.imgProduktuIcon}
+				src={ProductImage}
+				width={100}
+				height={100}
+				alt=""
+			/>
+
+			<div className={styles.frameParent}>
+				<div className={styles.frameGroup}>
+					<div className={styles.frameWrapper}>
+						<div className={styles.krainaNoteciParent}>
+							<b className={styles.krainaNoteci}>
+								KRAINA NOTECI
+							</b>
+
+							<div className={styles.karmaLoremIpsum}>
+								KARMA LOREM IPSUM 5KG
+							</div>
+						</div>
+					</div>
+
+					<div className={styles.zWrapper}>
+						<div className={styles.z}>178,00 zł</div>
+					</div>
+				</div>
+
+				<div className={styles.frameContainer}>
+					<div className={styles.parent}>
+						<div className={styles.div}>-</div>
+
+						<div className={styles.wrapper}>
+							<div className={styles.div2}>1</div>
+						</div>
+
+						<div className={styles.div3}>+</div>
+					</div>
+
+					<div className={styles.ulubioneParent}>
+						<FavoriteIcon className={styles.ulubioneIcon} />
+
+						<div className={styles.dodajDoKoszyka}>
+							<CartIcon className={styles.vectorIcon} />
+						</div>
+
+						<FavoriteIcon className={styles.ulubioneIcon} />
+					</div>
+				</div>
+			</div>
+		</div>
+	);
+};
+
+export default Property1Ulubione;
---
+++ zootopia/app/Liked/page.tsx
@@ -0,0 +1,43 @@
+// pages/ulubione/index.tsx
+
+import type { NextPage } from "next";
+import Link from "next/link";
+
+import Property1Ulubione from "./likedItem";
+
+
+import styles from "./index.module.css";
+
+const UlubionePage: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <h1 className={styles.title}>Polubione produkty</h1>
+
+      <div className={styles.list}>
+        <div className={styles.item}>
+          <Property1Ulubione />
+        </div>
+
+        <div className={styles.item}>
+          <Property1Ulubione />
+        </div>
+
+        <div className={styles.item}>
+          <Property1Ulubione />
+        </div>
+      </div>
+
+      <div className={styles.summary}>
+        <span>Razem:</span>
+        <span>478,00 zł</span>
+      </div>
+
+      <Link href="/" className={styles.backButton}>
+        <span className={styles.arrow}>‹</span>
+        Kontynuuj zakupy
+      </Link>
+    </div>
+  );
+};
+
+export default UlubionePage;
