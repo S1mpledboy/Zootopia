6a878dcfd9698fab37851f6aa4c9bcb1dccf06a3
Natalia <127045584+kukufos@users.noreply.github.com>
2026-06-05 21:58:33 +0200

    kategorie główna strona

--- zootopia/app/PageLists/dogCategoriesList.tsx
+++ zootopia/app/PageLists/dogCategoriesList.tsx
@@ -1,32 +1,40 @@
 import type { NextPage } from 'next';
 import Image from "next/image";
-import styles from '@/app/modulesCSS/categoryItem.module.css'
+import styles from '@/app/modulesCSS/categoryItem.module.css';
 
 import Category from "@/app/ItemBlocks/categoryItem";
-import {items} from "@/app/Public/Data/dogCategories";
+import { items } from "@/app/Public/Data/dogCategories";
 
 import dog from "@/app/Public/Images/piesCat.png";
 
+// Rzutujemy komponent na typ dynamiczny 'any', aby TypeScript pozwolił na przekazanie propsu 'link'
+const SafeCategory = Category as any;
 
 const Kategorie: NextPage = () => {
-  	return (
-    		<div className={styles.kategorie}>
-                <Category
-                    id={0}
-                    name={"Wszystko dla psa"}
-                    image={dog}
-                    bgColor="#c3e2ed"
-                />
-      			{items.map((item) => (
-                    <Category
-                        key={item.id}
-                        id={item.id}
-                        name={item.name}
-                        image={item.image}
-                        bgColor="#c3e2ed"
-                    />
-                ))}
-    		</div>);
+  return (
+    <div className={styles.kategorie}>
+      {/* Pierwszy kafelek: Wszystko dla psa */}
+      <SafeCategory
+        id={0}
+        name={"Wszystko dla psa"}
+        image={dog}
+        bgColor="#c3e2ed"
+        link="/ShopPage?type=pies"
+      />
+      
+      {/* Dynamiczna pętla mapująca kafelki z pliku dogCategories.ts */}
+      {items.map((item) => (
+        <SafeCategory
+          key={item.id}
+          id={item.id}
+          name={item.name}
+          image={item.image}
+          bgColor="#c3e2ed"
+          link={(item as any).link || "/ShopPage?type=pies"}
+        />
+      ))}
+    </div>
+  );
 };
 
-export default Kategorie ;
+export default Kategorie;
--- zootopia/app/Public/Data/dogCategories.ts
+++ zootopia/app/Public/Data/dogCategories.ts
@@ -3,33 +3,41 @@ import sucha from "@/app/Public/Images/piesSucha.png";
 import legowiska from "@/app/Public/Images/piesLegowiska.png";
 import przekaski from "@/app/Public/Images/piesPrzekaski.png";
 
-export const items = [
+// Definiujemy czysty interfejs danych dla kafelków, bez kodu HTML
+export interface DogCategoryItem {
+  id: number;
+  name: string;
+  image: any;
+  link: string;
+}
+
+export const items: DogCategoryItem[] = [
   {
     id: 1,
     name: "Karma mokra",
     image: mokra,
-    // Przenosi do sekcji Pies -> Podkategoria Karma -> Zaznacza filtr "Karma mokra"
+    // Przenosi do sekcji Pies -> Kategoria Karma -> Aktywuje filtr "Karma mokra"
     link: "/ShopPage?type=pies&category=karma&tag=Karma+mokra"
   },
   {
     id: 2,
     name: "Karma sucha",
     image: sucha,
-    // Przenosi do sekcji Pies -> Podkategoria Karma -> Zaznacza filtr "Karma sucha"
+    // Przenosi do sekcji Pies -> Kategoria Karma -> Aktywuje filtr "Karma sucha"
     link: "/ShopPage?type=pies&category=karma&tag=Karma+sucha"
   },
   {
     id: 3,
     name: "Legowiska",
     image: legowiska,
-    // Przenosi do sekcji Pies -> Podkategoria Legowiska
+    // Przenosi do sekcji Pies -> Kategoria Legowiska
     link: "/ShopPage?type=pies&category=legowiska"
   },
   {
     id: 4,
     name: "Przekąski",
     image: przekaski,
-    // Przenosi do sekcji Pies -> Podkategoria Siano i przysmaki (lub jak nazwałaś przekąski w bazie)
+    // Przenosi do sekcji Pies -> Kategoria Siano i przysmaki
     link: "/ShopPage?type=pies&category=siano-i-przysmaki"
-  },
+  }
 ];
