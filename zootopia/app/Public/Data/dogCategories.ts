d05d994bb3ae0f89459139d196bfc9af5e56e49c
Natalia <127045584+kukufos@users.noreply.github.com>
2026-06-05 21:50:02 +0200

    Update dogCategories.ts

--- zootopia/app/Public/Data/dogCategories.ts
+++ zootopia/app/Public/Data/dogCategories.ts
@@ -7,21 +7,29 @@ export const items = [
   {
     id: 1,
     name: "Karma mokra",
-    image: mokra
+    image: mokra,
+    // Przenosi do sekcji Pies -> Podkategoria Karma -> Zaznacza filtr "Karma mokra"
+    link: "/ShopPage?type=pies&category=karma&tag=Karma+mokra"
   },
   {
     id: 2,
     name: "Karma sucha",
-    image: sucha
+    image: sucha,
+    // Przenosi do sekcji Pies -> Podkategoria Karma -> Zaznacza filtr "Karma sucha"
+    link: "/ShopPage?type=pies&category=karma&tag=Karma+sucha"
   },
   {
     id: 3,
     name: "Legowiska",
-    image: legowiska
+    image: legowiska,
+    // Przenosi do sekcji Pies -> Podkategoria Legowiska
+    link: "/ShopPage?type=pies&category=legowiska"
   },
   {
     id: 4,
     name: "Przekąski",
-    image: przekaski
+    image: przekaski,
+    // Przenosi do sekcji Pies -> Podkategoria Siano i przysmaki (lub jak nazwałaś przekąski w bazie)
+    link: "/ShopPage?type=pies&category=siano-i-przysmaki"
   },
 ];
