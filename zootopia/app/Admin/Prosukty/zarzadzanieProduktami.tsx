1721d760a6ff4cf966065da10137e8ab22349262
Pjoter9c <pduczek9c@wp.pl>
2026-06-07 15:32:30 +0200

    css

--- zootopia/app/Admin/Prosukty/zarzadzanieProduktami.tsx
+++ zootopia/app/Admin/Prosukty/zarzadzanieProduktami.tsx
@@ -4,7 +4,7 @@ import Image from 'next/image';
 import Link from 'next/link';
 import { useSearchParams, useRouter } from 'next/navigation';
 import { useState, useMemo, useEffect } from 'react';
-import styles from './shopPage.module.css';
+import styles from './zarzadzanieProduktami.module.css';
 
 import arrowDown from '@/app/Public/Images/arrowDown.svg';
 import arrowRight from '@/app/Public/Images/arrowRight.svg';
